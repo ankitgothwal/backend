bdf9ed637cdd8638b0a31f66f2c159866f9a9679
ankitgothwal <ankitgothwal20@gmail.com>
2024-12-27 22:45:43 +0530

    add controller and routes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req,res,next)=> {
+    return (req,res,next)=> {
     console.log("Connected to database")
     Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
